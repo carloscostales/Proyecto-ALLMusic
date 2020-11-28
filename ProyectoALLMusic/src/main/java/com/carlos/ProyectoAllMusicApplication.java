84f55655b8278d2707cb54af207db276658904e6
carlosdaw04 <costales98@gmail.com>
2020-11-28 16:50:37 +0100

    .jar a .war

--- ProyectoALLMusic/src/main/java/com/carlos/ProyectoAllMusicApplication.java
+++ ProyectoALLMusic/src/main/java/com/carlos/ProyectoAllMusicApplication.java
@@ -2,12 +2,19 @@ package com.carlos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProyectoAllMusicApplication {
+public class ProyectoAllMusicApplication extends SpringBootServletInitializer  {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(ProyectoAllMusicApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoAllMusicApplication.class, args);
 	}
-
+	
 }
