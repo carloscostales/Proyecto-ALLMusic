635d6d83a4a505b084514b2fc0f10f401340734a
carlosdaw04 <costales98@gmail.com>
2020-12-04 12:17:01 +0100

    Animación en los modales.

--- ProyectoALLMusic/src/main/resources/static/js/script.js
+++ ProyectoALLMusic/src/main/resources/static/js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-    
+
+    /* ANIMACION DE TODOS LOS MODALES */
+    $('.modal').on('show.bs.modal', function (e) {
+		$('.modal .modal-dialog').attr('class', 'modal-dialog animate__animated animate__bounceInDown animate__faster');
+	})
+	$('.modal').on('hide.bs.modal', function (e) {
+	   	$('.modal .modal-dialog').attr('class', 'modal-dialog animate__animated animate__bounceOutDown animate__faster');
+		$('.fade').css('transition', 'opacity 0.4s linear');
+	})
 });
 
+/* FUNCIÓN QUE BLOQUEA LA ENTRADA DE LETRAS EN EL INPUT DE LA FECHA */
 function validateDate(evt){
 	var charCode = (evt.which) ? evt.which : event.keyCode
 	
